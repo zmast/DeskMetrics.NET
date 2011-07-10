ee082176fdab2fe789205b84781046f2095240c6
Herberth Amaral <herberthamaral@gmail.com>
2011-07-10 18:49:01 -0300

    Fixing exception json generation

--- Json/ExceptionJson.cs
+++ Json/ExceptionJson.cs
@@ -34,9 +34,18 @@ namespace DeskMetrics.Json
         {
             var json = base.GetJsonHashTable();
             json.Add("msg", Exception.Message.Trim().Replace("\r\n", "").Replace("  ", " ").Replace("\n", "").Replace(@"\n", "").Replace("\r", "").Replace("&", "").Replace("|", "").Replace(">", "").Replace("<", "").Replace("\t", "").Replace(@"\", @"/"));
-            json.Add("stk", Exception.StackTrace.Trim().Replace("\r\n", "").Replace("  ", " ").Replace("\n", "").Replace(@"\n", "").Replace("\r", "").Replace("&", "").Replace("|", "").Replace(">", "").Replace("<", "").Replace("\t", "").Replace(@"\", @"/"));
-            json.Add("src", Exception.Source.Trim().Replace("\r\n", "").Replace("  ", " ").Replace("\n", "").Replace(@"\n", "").Replace("\r", "").Replace("&", "").Replace("|", "").Replace(">", "").Replace("<", "").Replace("\t", "").Replace(@"\", @"/"));
-            json.Add("tgs", Exception.TargetSite.ToString());
+            if (Exception.StackTrace != null)
+				json.Add("stk", Exception.StackTrace.Trim().Replace("\r\n", "").Replace("  ", " ").Replace("\n", "").Replace(@"\n", "").Replace("\r", "").Replace("&", "").Replace("|", "").Replace(">", "").Replace("<", "").Replace("\t", "").Replace(@"\", @"/"));
+			else
+				json.Add("stk","");
+			if (Exception.Source != null)
+            	json.Add("src", Exception.Source.Trim().Replace("\r\n", "").Replace("  ", " ").Replace("\n", "").Replace(@"\n", "").Replace("\r", "").Replace("&", "").Replace("|", "").Replace(">", "").Replace("<", "").Replace("\t", "").Replace(@"\", @"/"));
+			else
+				json.Add("src","");
+            if (Exception.TargetSite!=null)
+				json.Add("tgs", Exception.TargetSite.ToString());
+			else
+				json.Add("tgs","");
             json.Add("fl", Flow);
             return json;
         }
